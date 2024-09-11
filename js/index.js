b664e66e6fb8d4cc2fdac5245e3a19ffe332188e
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2024-09-11 11:33:38 -0300

    Prueba de datos

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded"), () => {
+
+    let datoIngresado = localStorage.getItem("inputText");
+
+    document.getElementById("buttonText").addEventListener("click", () => {
+    let datoAMostrar = document.getElementById("inputText");
+});
+}
+
